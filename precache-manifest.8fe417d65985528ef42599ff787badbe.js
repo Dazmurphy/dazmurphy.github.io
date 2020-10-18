4adc339754621970721f6a920bc608413ba6cff0
dazmurphy <daz.murphy.94@gmail.com>
2020-10-18 11:44:51 +0100

    Updates

---
+++ precache-manifest.8fe417d65985528ef42599ff787badbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae14e8adf73d6864704a11fea5cf81be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2634a86628b78c647c4",
+    "url": "/static/js/2.1ce69071.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.1ce69071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8024fa630a1f7ab7f154",
+    "url": "/static/js/main.142a737c.chunk.js"
+  },
+  {
+    "revision": "2c3b525dae1f768724ec",
+    "url": "/static/js/runtime-main.7a6af732.js"
+  }
+]);
